983cea29a47e4738bc43d47e8699cd09eeb61576
Wukongthethird <simonzhang9518@gmail.com>
2021-07-16 11:19:48 -0700

    pet model

--- models.py
+++ models.py
@@ -2,6 +2,7 @@
 
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 
@@ -13,3 +14,50 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.Model):
+    """Pet """
+
+    __tablename__ = "pets"
+
+    id = db.Column( 
+                    db.Integer,
+                    Primary_Key=True, 
+                    nullable = False, 
+                    autoincrement = True
+                    )
+
+    name = db.Column(
+                    db.Text,
+                    nullable = False
+
+                    )
+
+    species = db.Column(
+                    db.Text,
+                    nullable = False
+                        )
+
+    photo_url = db.Columnn(
+                    db.Text,
+                    nullable = False,
+                    default = ''
+                        )
+
+    age = db.Column(
+                    db.Text, 
+                    nullable = False,
+                        # .in_([ 'Baby' , 'Young', 'Adult', 'Senior' ])
+
+                     )
+
+    notes = db.column(
+                    db.Text
+                    )
+    
+    available =  db.column(
+                        db.Boolean ,
+                        nullable = False,
+                        default = True
+                    )
